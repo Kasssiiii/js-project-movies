b9c4eb77acce5b28409c3ab2ebb3a9e6109b27a7
Kasssiiii <kasia.gru@gmail.com>
2025-06-07 20:06:56 +0200

    movie details styling and formatting

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
 
+import "./MovieDetails.css";
+
 export const MovieDetails = () => {
   const params = useParams();
   const [movieDetails, setMovieDetails] = useState({});
@@ -8,6 +10,8 @@ export const MovieDetails = () => {
   const movieId = params.movieId;
 
   const apiURI = `https://api.themoviedb.org/3/movie/${movieId}`;
+
+  const imageLocation = "http://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     fetch(`${apiURI}?api_key=${apiKey}`)
@@ -18,12 +22,38 @@ export const MovieDetails = () => {
 
   if (movieDetails) {
     return (
-      <>
-        <Link to="/">Go Back</Link>
-        <p>Title: {movieDetails.title}</p>
-        <p>Stars: {movieDetails.vote_average}</p>
-        <p>Description: {movieDetails.overview}</p>
-      </>
+      <div className="detailContainer">
+        <img
+          src={`${imageLocation}${movieDetails.backdrop_path}`}
+          alt={movieDetails.title}
+          className="backgroundImage"
+        />
+        <Link className="arrow" to="/">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+            <path
+              d="M27 14.5C27 7.596441 21.4035594 2 14.5 2S2 7.596441 2 14.5 7.5964406 27 14.5 27 27 21.403559 27 14.5zm-19.3388348-.353553l7.4852814-7.485282c.1952622-.195262.5118446-.195262.7071068 0l2.1213203 2.121321c.1952622.195262.1952622.511844 0 .707106L12.9644661 14.5l5.0104076 5.010408c.1952622.195262.1952622.511844 0 .707106l-2.1213203 2.121321c-.1952622.195262-.5118446.195262-.7071068 0l-7.4852814-7.485282c-.19799-.19799-.197989-.509117 0-.707106z"
+              fill="#fff"
+              fillRule="evenodd"
+            ></path>
+          </svg>{" "}
+          <h3 className="arrowTitle">Movies</h3>
+        </Link>
+        <div className="summary">
+          <img
+            src={`${imageLocation}${movieDetails.poster_path}`}
+            alt={movieDetails.title}
+            className="detailImage"
+          />
+
+          <div className="details">
+            <h2>
+              <span className="title">{movieDetails.title+" "}</span>
+              <div className="rating"> ⭐️{movieDetails.vote_average} </div>
+            </h2>
+            <p>{movieDetails.overview}</p>
+          </div>
+        </div>
+      </div>
     );
   }
 
