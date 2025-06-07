ab56948327af9647dbccbef15b5a06a40525a715
Kasssiiii <kasia.gru@gmail.com>
2025-06-07 21:14:42 +0200

    styling of cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router";
 
+import "./MovieCard.css";
+
 export const MovieCard = ({ movie }) => {
+  const imageLocation = "http://image.tmdb.org/t/p/w500/";
   return (
-    <Link to={`/movies/${movie.id}`}>
-      <p>
-        {movie.title}, released: {movie.release_date}
-      </p>
+    <Link className="card" to={`/movies/${movie.id}`}>
+      <img
+        src={`${imageLocation}${movie.poster_path}`}
+        alt={movie.title}
+        className="mainImage"
+      />
+      <div className="info">
+        <h1>{movie.title}</h1>
+        <p>Released: {movie.release_date}</p>
+      </div>
     </Link>
   );
 };
